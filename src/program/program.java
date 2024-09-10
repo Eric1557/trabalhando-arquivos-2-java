01e1da3fcb043c58bc553ee1d8798b7c2d50b01c
Eric1557 <ericbbarbosa2019@gmail.com>
2024-09-10 10:23:17 -0300

    fileReader and BufferedReader

--- src/program/program.java
+++ src/program/program.java
@@ -1,32 +1,46 @@
 package program;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
+
 
 public class program {
 
 	public static void main(String[] args) {
 
-		File file = new File("C:\\Users\\eric-\\OneDrive\\Documentos\\ERIC CURSO\\PROGRAMAÇAO\\JAVA\\program\\in.txt");
-		Scanner sc = null;
+		String path = "C:\\Users\\eric-\\OneDrive\\Documentos\\ERIC CURSO\\PROGRAMAÇAO\\JAVA\\program\\in.txt";
+		FileReader fr = null;
+		BufferedReader br = null;
 
 		try {
-			sc = new Scanner(file);
-			if (sc.hasNextLine()) {
-				System.out.println(sc.nextLine());
+			fr = new FileReader(path);
+			br = new BufferedReader(fr);
+
+			String line = br.readLine();
+			while (line != null) {
+				System.out.println(line);
+				line = br.readLine();
 
 			}
-		} catch (IOException e) {
-			System.out.println("error : " + e.getMessage());
 
+		} catch (IOException e) {
+			System.out.println("error " + e.getMessage());
 		}
 
 		finally {
-			if(sc != null) {
-				sc.close();
+			try {
+				if (br != null) {
+					br.close();
+				}
+				if (fr != null) {
+					fr.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
-		}
-	}
 
+		}
+
+	}
 }
